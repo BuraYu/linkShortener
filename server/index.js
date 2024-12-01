80e4d05d77360e9e9761d843357705f4d79518af
BuraYu <burak.yueksel@hotmail.com>
2024-12-01 17:22:40 +0100

    add dotenv mongoose connection to db

--- server/index.js
+++ server/index.js
@@ -1,13 +1,39 @@
+require("dotenv").config();
+
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const mongoose = require("mongoose");
 
 app.use(cors());
 const port = 3030;
 
-app.listen(port, console.log("server is running"));
 
 app.get("/", (req, res) => {
   res.send("Server is running");
   console.log("Req answered");
 });
+
+console.log();
+
+mongoose
+  .connect(process.env.ATLASURI)
+  .then(() => {
+    console.log("connected to the db");
+    app.listen(port, console.log("Server is running"));
+  })
+  .catch((error) => {
+    console.log("not connected to db");
+  });
+
+// original URL
+// shortened URL
+// ID
+// creation date and Time
+// experation date
+
+// analytic:
+// click count
+// last accessed
+
+// status (live, deleted, expired)
