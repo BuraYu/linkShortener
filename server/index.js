15d95b848d21133d381745183f3c133633410338
BuraYu <burak.yueksel@hotmail.com>
2024-12-01 19:18:11 +0100

    add finding by id function

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const cors = require("cors");
 const app = express();
 const mongoose = require("mongoose");
 const LinkModel = require("./models/link.model.js");
+const { ObjectId } = require("mongodb");
 
 app.use(cors());
 app.use(express.json());
@@ -14,6 +15,26 @@ const port = 3030;
 app.get("/", (req, res) => {
   res.send("Server is running");
   console.log("Req answered");
+});
+
+app.get("/api/link", async (req, res) => {
+  try {
+    const allLinks = await LinkModel.find({});
+    res.status(200).json({ allLinks });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+  console.log(req.body);
+});
+
+app.get("/api/link/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const link = await LinkModel.findById(new ObjectId(id));
+    res.status(200).json({ link });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
 
 app.post("/api/link", async (req, res) => {
