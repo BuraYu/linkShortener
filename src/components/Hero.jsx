5ef6ee4e55b9cd3fad4805e6d25c31da807f7866
BuraYu <burak.yueksel@hotmail.com>
2024-12-03 23:19:58 +0100

    add hero section component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,24 @@
 import React from "react";
 import "./hero.css";
+import workingIllustration from "../assets/illustration-working.svg";
 
 export const Hero = () => {
   return (
-    <div>
-      <h1 className="hero">Hero</h1>
+    <div className="hero__container">
+      <div className="hero__description">
+        <h1>More than just shorter links</h1>
+        <p>
+          Build your brand’s recognition and get detailed insights on how your
+          links are performing.
+        </p>
+        <span>
+          <a href="#">Get Started</a>
+        </span>
+      </div>
+      <img
+        src={workingIllustration}
+        alt="illustration of a person sitting at a computer"
+      />
     </div>
   );
 };
